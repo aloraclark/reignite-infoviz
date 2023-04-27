f6afd8d9f10ba95b19ae93f933ee8ca74db17d3b
aloraclark <65735963+aloraclark@users.noreply.github.com>
2023-04-26 22:20:24 -0700

    added bar chart

---
+++ common-num-donations.js
@@ -0,0 +1,79 @@
+let labels = [
+    '1', 
+    '2', 
+    '3', 
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '10',
+    '11',
+    '12',
+    '18',
+    '20',
+    '30+'
+];
+
+let itemData = [
+    13, 
+    6, 
+    7, 
+    4,
+    1,
+    5,
+    1,
+    3,
+    2,
+    1,
+    2,
+    1,
+    1,
+    5
+];
+
+let barColors = [
+    'rgb(81, 151, 176)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)'
+]
+
+const data = {
+    labels: labels,
+    datasets: [{
+        data: itemData,
+        backgroundColor: barColors
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: 'Number of Times People Have Donated in the Last Six Months'
+            }
+        }
+    }
+};
+
+const chart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
