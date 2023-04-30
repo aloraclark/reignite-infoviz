7f573d05387152f87f96697a3999daff69ba5239
aloraclark <65735963+aloraclark@users.noreply.github.com>
2023-04-30 00:03:42 -0700

    finished toggle screen for more info

---
+++ scripts/hid-and-show-div.js
@@ -0,0 +1,8 @@
+function hideAndShow() {
+  var x = document.getElementById("student_div");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
