b76bb02fe343b826f82f18e0ca5221dd4f492256
aloraclark <65735963+aloraclark@users.noreply.github.com>
2023-04-30 15:01:14 -0700

    finished horizontal bar chart

---
+++ scripts/how-donors-find-npos.js
@@ -0,0 +1,53 @@
+let labels2 = [
+    'Social Media', 
+    'Family or Friends', 
+    'Personal or Professional Affiliation', 
+    'Online Research',
+    'Other'
+];
+
+let itemData2 = [
+    11, 
+    21, 
+    35, 
+    15,
+    11,
+];
+
+let barColors2 = [
+    'rgb(82, 98, 104)',
+    'rgb(18, 176, 218)',
+    'rgb(18, 176, 218)',
+    'rgb(82, 98, 104)',
+    'rgb(82, 98, 104)',
+]
+
+const data2 = {
+    labels: labels2,
+    datasets: [{
+        data: itemData2,
+        backgroundColor: barColors2
+    }]
+};
+
+const config2 = {
+    type: 'bar',
+    data: data2,
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: 'Means Through Which People Find NPOs'
+            }
+        },
+        indexAxis: 'y'
+    }
+};
+
+const chart2 = new Chart(
+    document.getElementById('horizBarChart'),
+    config2
+);
